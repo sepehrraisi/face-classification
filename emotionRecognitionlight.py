34ad383dd0ca439d4fc88e584f9bc3c680547126
Sepehr Raisi <sepehr.raisi@gmail.com>
2022-08-30 10:54:12 +0430

    Added light emotion detection.

---
+++ emotionRecognitionlight.py
@@ -0,0 +1,96 @@
+# Importing required packages
+from keras.models import load_model
+import numpy as np
+import argparse
+import dlib
+import cv2
+
+emotion_offsets = (20, 40)
+emotions = {
+    0: {
+        "emotion": "Angry",
+        "color": (193, 69, 42)
+    },
+    1: {
+        "emotion": "Disgust",
+        "color": (164, 175, 49)
+    },
+    2: {
+        "emotion": "Fear",
+        "color": (40, 52, 155)
+    },
+    3: {
+        "emotion": "Happy",
+        "color": (23, 164, 28)
+    },
+    4: {
+        "emotion": "Sad",
+        "color": (164, 93, 23)
+    },
+    5: {
+        "emotion": "Suprise",
+        "color": (218, 229, 97)
+    },
+    6: {
+        "emotion": "Neutral",
+        "color": (108, 72, 200)
+    }
+}
+
+
+def shapePoints(shape):
+    coords = np.zeros((68, 2), dtype="int")
+    for i in range(0, 68):
+        coords[i] = (shape.part(i).x, shape.part(i).y)
+    return coords
+
+
+def rectPoints(rect):
+    x = rect.left()
+    y = rect.top()
+    w = rect.right() - x
+    h = rect.bottom() - y
+    return (x, y, w, h)
+
+
+faceLandmarks = "faceDetection/models/dlib/shape_predictor_68_face_landmarks.dat"
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(faceLandmarks)
+
+emotionModelPath = 'models/emotionModel.hdf5'  # fer2013_mini_XCEPTION.110-0.65
+emotionClassifier = load_model(emotionModelPath, compile=False)
+emotionTargetSize = emotionClassifier.input_shape[1:3]
+
+cap = cv2.VideoCapture(0)
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        break
+
+    frame = cv2.resize(frame, (720, 480))
+    grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    rects = detector(grayFrame, 0)
+    for rect in rects:
+        shape = predictor(grayFrame, rect)
+        points = shapePoints(shape)
+        (x, y, w, h) = rectPoints(rect)
+        grayFace = grayFrame[y:y + h, x:x + w]
+        try:
+            grayFace = cv2.resize(grayFace, (emotionTargetSize))
+        except:
+            continue
+
+        grayFace = grayFace.astype('float32')
+        grayFace = grayFace / 255.0
+        grayFace = (grayFace - 0.5) * 2.0
+        grayFace = np.expand_dims(grayFace, 0)
+        grayFace = np.expand_dims(grayFace, -1)
+        emotion_prediction = emotionClassifier.predict(grayFace)
+        emotion_probability = np.max(emotion_prediction)
+        if (emotion_probability > 0.36):
+            emotion_label_arg = np.argmax(emotion_prediction)
+            print(emotions[emotion_label_arg]['emotion'])
+
+cap.release()
+cv2.destroyAllWindows()
