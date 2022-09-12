2d508317fd48486e26cb243a0bfa2e741af2c1f1
Sepehr Raisi <sepehr.raisi@gmail.com>
2022-09-12 11:45:45 +0430

    Fix LED.

--- led_test.py
+++ led_test.py
@@ -1,13 +1,7 @@
-import re
 import time
-import argparse
-
 from luma.led_matrix.device import max7219
 from luma.core.interface.serial import spi, noop
 from luma.core.render import canvas
-from luma.core.virtual import viewport
-from luma.core.legacy import text, show_message
-from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT
 
 serial = spi(port=0, device=0, gpio=noop())
 device = max7219(serial, cascaded=2, block_orientation=-90,
