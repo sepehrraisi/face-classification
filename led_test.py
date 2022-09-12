5d00b081a453b2ac353b4ebd753e3f4e48c69430
Sepehr Raisi <sepehr.raisi@gmail.com>
2022-09-12 10:13:55 +0430

    Fix LED.

--- led_test.py
+++ led_test.py
@@ -9,4 +9,11 @@ from luma.core.virtual import viewport
 from luma.core.legacy import text, show_message
 from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT
 
+serial = spi(port=0, device=0, gpio=noop())
+device = max7219(serial, cascaded=2, block_orientation=-90,
+                     rotate=0, blocks_arranged_in_reverse_order=False)
+device.contrast(8 * 16)
+draw.rounded_rectangle(device.bounding_box, outline="white")
+time.sleep(0.5)
+device.contrast(16 * 16)
 draw.rounded_rectangle(device.bounding_box, outline="white")
