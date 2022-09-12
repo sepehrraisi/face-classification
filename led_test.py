f3301997de04682f9e9596903f67fcddf5a47d6c
Sepehr Raisi <sepehr.raisi@gmail.com>
2022-09-12 10:12:12 +0430

    Fix LED.

--- led_test.py
+++ led_test.py
@@ -1,4 +1,12 @@
-import led
+import re
+import time
+import argparse
 
-print("Smile")
-led.smile()
+from luma.led_matrix.device import max7219
+from luma.core.interface.serial import spi, noop
+from luma.core.render import canvas
+from luma.core.virtual import viewport
+from luma.core.legacy import text, show_message
+from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT
+
+draw.rounded_rectangle(device.bounding_box, outline="white")
