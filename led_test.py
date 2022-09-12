c63c792c8f5d7f6470ec44ab743f1d425998debd
Sepehr Raisi <sepehr.raisi@gmail.com>
2022-09-12 11:11:38 +0430

    Fix LED.

--- led_test.py
+++ led_test.py
@@ -13,27 +13,27 @@ serial = spi(port=0, device=0, gpio=noop())
 device = max7219(serial, cascaded=2, block_orientation=-90,
                  rotate=0, blocks_arranged_in_reverse_order=False)
 
-with canvas(device) as draw:
-    draw.arc((0, -2, 15, 7), -10, 180, fill="white", width=2)
+# with canvas(device) as draw:
+#     draw.arc((0, -2, 15, 7), -10, 180, fill="white", width=2)
+#
+# for _ in range(2):
+#     for intensity in range(16):
+#         device.contrast(intensity * 16)
+#         time.sleep(0.1)
 
-for _ in range(2):
-    for intensity in range(16):
-        device.contrast(intensity * 16)
-        time.sleep(0.1)
-
 with canvas(device) as draw:
-    draw.arc((0, 2, 15, 9), 170, 8, fill="white", width=2)
+    draw.arc((0, 2, 15, 9), 170, 12, fill="white", width=2)
     print(device.bounding_box)
 
-for _ in range(2):
+for _ in range(5):
     for intensity in range(16):
         device.contrast(intensity * 16)
         time.sleep(0.1)
 
-with canvas(device) as draw:
-    draw.line(device.bounding_box, fill="white", width=2)
-
-for _ in range(2):
-    for intensity in range(16):
-        device.contrast(intensity * 16)
-        time.sleep(0.1)
+# with canvas(device) as draw:
+#     draw.line(device.bounding_box, fill="white", width=2)
+#
+# for _ in range(2):
+#     for intensity in range(16):
+#         device.contrast(intensity * 16)
+#         time.sleep(0.1)
