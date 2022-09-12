de511b0cbfc76e6148e2460fd706f144fed3d8c2
Sepehr Raisi <sepehr.raisi@gmail.com>
2022-09-12 11:40:19 +0430

    Fix LED.

--- led_test.py
+++ led_test.py
@@ -17,9 +17,9 @@ with canvas(device) as draw:
     with canvas(device) as draw:
         draw.rectangle((0, 3, 15, 4), fill="white", width=1)
 for _ in range(2):
-        for intensity in range(16):
-            device.contrast(intensity * 16)
-            time.sleep(0.1)
+    for intensity in range(16):
+        device.contrast(intensity * 16)
+        time.sleep(0.1)
 
 with canvas(device) as draw:
     draw.arc((0, 0, 15, 7), -10, 200, fill="white", width=2)
