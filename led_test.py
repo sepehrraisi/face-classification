9f0af92d5c5dda8c18108fcfcd38072b789a2e91
Sepehr Raisi <sepehr.raisi@gmail.com>
2022-09-12 10:30:05 +0430

    Fix LED.

--- led_test.py
+++ led_test.py
@@ -14,7 +14,7 @@ device = max7219(serial, cascaded=2, block_orientation=-90,
                      rotate=0, blocks_arranged_in_reverse_order=False)
 
 with canvas(device) as draw:
-    draw.arc(device.bounding_box, 0, 72, fill="white")
+    draw.arc(device.bounding_box, 0, 128, fill="white")
 
 for _ in range(5):
     for intensity in range(16):
